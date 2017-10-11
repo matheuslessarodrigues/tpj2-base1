2b58d72eef86216e8c7309bcc890dc2f16ba4521
Matheus Lessa <matheuslessarodrigues@gmail.com>
2017-10-10 23:07:20 -0300

    meio que movimento

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public sealed class PlayerMovement : MonoBehaviour
+{
+	public Rigidbody2D playerRigidbody;
+	public PhysicsMaterial2D standingMaterial;
+	public PhysicsMaterial2D movingMaterial;
 
-	// Use this for initialization
-	void Start () {
-		
+	public float moveSpeed = 1.0f;
+	public float maxHorizontalVelocity = 10.0f;
+	public float jumpImpulse = 10.0f;
+
+	private void Update()
+	{
+		if( Input.GetButtonDown( "Jump" ) )
+			playerRigidbody.AddForce( new Vector2( 0.0f, jumpImpulse ), ForceMode2D.Impulse );
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void FixedUpdate()
+	{
+		float moveInput = Input.GetAxis( "Horizontal" );
+
+		if( Mathf.Abs( moveInput ) > Mathf.Epsilon )
+		{
+			if( Mathf.Abs( playerRigidbody.velocity.x ) < maxHorizontalVelocity )
+			{
+				float move = moveInput * moveSpeed;
+				playerRigidbody.AddForce( new Vector2( move, 0.0f ), ForceMode2D.Force );
+			}
+
+			playerRigidbody.sharedMaterial = movingMaterial;
+		}
+		else
+		{
+			playerRigidbody.sharedMaterial = standingMaterial;
+		}
 	}
 }
